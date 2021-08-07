f90f69373d1b45d03ce2d15d2b07e16ec52fce2b
Rafael Estrada <rafaelestrada@comcast.net>
2021-08-06 22:29:50 -0700

    added fade to banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import axios from './axios';
 import requests  from './requests';
+import './Banner.css'
 
 function Banner() {
 
@@ -20,6 +21,10 @@ function Banner() {
      fetchData();
  }, []);
 console.log(movie);
+function truncate(str,n){
+    return str?.length>n ? str.substr(0,n-1) + '...' : str;
+}
+
 
     return (
         <header className='banner'
@@ -30,7 +35,10 @@ console.log(movie);
             backgroundPosition:'center center',
         }}>
           <div className='banner__contents'>
-              <h1>
+         
+              <h1 className='banner__title'>
+            
+              
                   {movie?.title || movie?.name || movie?.original_name}
               </h1>
               <div
@@ -39,12 +47,11 @@ console.log(movie);
                   <button className="banner__button">My List</button>
 
               </div>
+              <h1 className="banner__description">{truncate(movie?.overview,150)}</h1>
 
           </div>
-            {''}
-            {/* title*/}
-            {/*div >2 buttons*/}
-            {/* description*/}
+         
+          <div className="banner--fadeBottom"/>
         </header>
     )
 }
